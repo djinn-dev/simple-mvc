701ea4dfc7a5f4e4b3261759027c8bee0989a6ed
Trevor <trevor.geene@gmail.com>
2021-01-24 14:28:58 -0800

    Fix Order of File Setup

--- system/loaders/config.php
+++ system/loaders/config.php
@@ -1,8 +1,8 @@
 <?php
-// Load Config Files
+// Setup Environment Vars
 require_once APP_PATH . '/config/development.php';
+require_once SYSTEM_PATH . '/misc/env.php';
+
+// Load Config Files
 require_once APP_PATH . '/config/routes.php';
 require_once APP_PATH . '/config/site.php';
-
-// Setup Environment Vars
-require_once SYSTEM_PATH . '/misc/env.php';
